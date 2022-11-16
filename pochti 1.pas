d15e551633adddde19755a9a77a4849da4412984
ALEKCZY <117628567+ALEKCZY@users.noreply.github.com>
2022-11-16 17:32:40 +0300

    Add files via upload

---
+++ pochti 1.pas
@@ -0,0 +1,24 @@
+﻿begin
+var A:array of integer;
+var s: integer;
+  write('Размер = ');
+var n:=readInteger;
+SetLength(A, n);
+  for var i:=0 to n-1 do
+  begin
+  A[i]:= readInteger;
+  end;
+  writeln;
+  write('Рандом число: ');
+  readln(s);
+ var q:=0;
+ var k:=0;
+  while (k<n) and (q<=s) do
+  begin
+    inc(k);
+    q:=q+A[k];
+  end; 
+  if q<=s then WriteLn('Сумма всех элементов = ',q,' < ',s)
+  else
+    WriteLn(k);
+end.
---
+++ pochti 2.pas
@@ -0,0 +1,32 @@
+﻿begin
+var s,ss: string;
+var l,ls,i,j: integer;
+var f: boolean;
+ readln(s);
+ readln(ss);
+ i := 1;
+ l := Length(s);
+ ls := Length(ss);
+ f := false;
+ while i <= l - ls + 1 do
+ begin
+   if s[i] = ss[1] then
+   begin
+     f := true;
+     for j := 1 to ls - 1 do
+       if s[i + j] <> ss[j + 1] then
+       begin
+         i := i + j;
+         f := false;
+         break
+       end;
+     if f then
+     begin
+       print('является');
+       exit;
+     end;
+   end;
+   i += 1;
+ end;
+ print('не является');
+end.
